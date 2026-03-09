638ec3c06cb3148f038889307f9e719121a7ff37
ec4t3rina <ecaterina@stefanescu.eu>
2026-03-10 00:25:21 +0100

    Add files via upload

---
+++ tema1/5.2.1.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int* vec;
+int size = 0;
+int capacity = 0;
+
+void init() {
+    vec = new int[2];
+    capacity = 2;
+}
+
+int get_size() {
+    return size;
+}
+
+void pop_back() {
+    size--;
+}
+
+void resize() {
+    int i;
+    int* newvec = new int[2*capacity];
+
+    capacity *= 2;
+    for (i=0; i<size; i++) {
+        newvec[i] = vec[i];
+    }
+
+    delete[] vec;
+    vec = newvec;
+    capacity *= 2;
+}
+
+void push_back(int x) {
+    if (size==capacity) {
+        resize();
+    }
+    vec[size] = x;
+    size++;
+}
+
+vector<int> v;
+
+int main() {
+    int i, n;
+
+    init();
+
+    push_back(1);
+    push_back(2);
+    push_back(4);
+    push_back(8);
+
+    cout << get_size() << '\n';
+
+    pop_back();
+    cout << get_size() << '\n';
+
+    n = get_size();
+    for (i=0; i<n; i++) {
+        cout << vec[i] << " ";
+    }
+
+    return 0;
+}
